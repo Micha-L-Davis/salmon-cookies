f3996a096c76460177fa292e55f808c790af38a0
Brady Camp <bradyjcamp@gmail.com>
2022-05-19 21:23:08 -0400

    implemented table data rendering

--- pages/_app.js
+++ pages/_app.js
@@ -2,28 +2,13 @@ import TableHeader from '../components/TableHeader';
 import '../styles/globals.css'
 
 
-// eslint-disable-next-line no-unused-vars
-const seattle = new Store('Seattle', 23, 65, 6.3);
-// eslint-disable-next-line no-unused-vars
-const tokyo = new Store('Tokyo', 3, 24, 1.2);
-// eslint-disable-next-line no-unused-vars
-const dubai = new Store('Dubai', 11, 38, 2.3);
-// eslint-disable-next-line no-unused-vars
-const paris = new Store('Paris', 20, 38, 2.3);
-// eslint-disable-next-line no-unused-vars
-const lima = new Store('Lima', 2, 16, 4.6);
-
-
 
 
 function App({ Component, pageProps }) {
   return (
-    <table>
-      <TableHeader />
-      <tbody>
-        {/* here there be loops */}
-      </tbody>
-    </table>
+    <div>
+    <Component {...pageProps} />
+    </div>
   );
 }
 
--- pages/sales.js
+++ pages/sales.js
@@ -1,7 +1,9 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import Script from 'next/script'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import Script from "next/script";
+import styles from "../styles/Home.module.css";
+import TableHeader from "../components/TableHeader";
+import SalesData from "../components/SalesData";
 
 export default function Sales() {
   return (
@@ -9,12 +11,17 @@ export default function Sales() {
       <Head>
         <title>Salmon Cookies Daily Sales Data</title>
       </Head>
-
-      <body>
         <header>
           <div>
             <div id="logo">
-              <a href="sales.html"><Image src="/../public/img/salmon.png" alt="a salmon" width="200px" height="200px" /></a>
+              <a href="sales.html">
+                <Image
+                  src="/../public/img/salmon.png"
+                  alt="a salmon"
+                  width="200px"
+                  height="200px"
+                />
+              </a>
             </div>
             <h1>Salmon Cookies</h1>
           </div>
@@ -22,29 +29,43 @@ export default function Sales() {
             <a href="index.html">Go back...</a>
           </nav>
         </header>
-        <section id="sales-table"></section>
         <section id="form">
           <form id="new-store-form">
             <fieldset>
               <legend>Add/Update a store location</legend>
-              <label>Location
+              <label>
+                Location
                 <input type="text" name="location" required="required" />
               </label>
-              <label>Min. Customers per Hour
+              <label>
+                Min. Customers per Hour
                 <input type="number" name="minCust" required="required" />
               </label>
-              <label>Max. Customers per Hour
+              <label>
+                Max. Customers per Hour
                 <input type="number" name="maxCust" required="required" />
               </label>
-              <label>Avg. Cookies per Sale
-                <input type="float" name="avgCookiePerSale" required="required" />
+              <label>
+                Avg. Cookies per Sale
+                <input
+                  type="float"
+                  name="avgCookiePerSale"
+                  required="required"
+                />
               </label>
             </fieldset>
             <button type="submit">Update Table</button>
           </form>
         </section>
-        <Script src="_app.js" />
-      </body>
+        <section id="sales-table">
+          <table style={{}}>
+            <thead>
+              <TableHeader />
+            </thead>
+          <SalesData />
+          </table>
+        </section>
+        {/* <Script src="_app.js" /> */}
     </div>
   );
 }
